237b9a946b47a5c18d80f077f8220f0f70c09d00
Alex Triana <alex.triana2@gmail.com>
2015-07-03 10:33:00 -0700

    type

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
 	var cur,
 		i=1,
 	items = [
-	"\"Thank you so much for all of the help you have given our son as he embarks on this college search. It has been invaluable\". -AnchorPrep Parent",
-	"My son was accepted to a top tier university and AnchorPrep deserves a ton of credit for helping to make his dream a reality.\" -AnchorPrep Parent",
+	"\"Thank you so much for all of the help you have given our son as he embarks on this college search. It has been invaluable.\" -AnchorPrep Parent",
+	"\"My son was accepted to a top tier university and AnchorPrep deserves a ton of credit for helping to make his dream a reality.\" -AnchorPrep Parent",
 	"\"Without AnchorPrep I wouldn't have been admitted to such amazing universities.\" -AnchorPrep Student",
-	"\" Your patience, wisdom, guidance and humor were much appreciated through the journey.\" -AnchorPrep Parent"]
+	"\"Your patience, wisdom, guidance and humor were much appreciated through the journey.\" -AnchorPrep Parent"]
 	setInterval(function(){ 
 		i+=1;
 		if (i == 4) {
