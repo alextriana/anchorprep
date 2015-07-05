6cf1f73d70c1ace640f4e81a71f73ebe8992372a
Alex Triana <alex.triana2@gmail.com>
2015-07-05 10:28:13 -0700

    sameorigin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :allow_iframe_requests
+  before_filter :iframe_action
 
   def after_sign_in_path_for(user)
   	admin_dashboard_path
