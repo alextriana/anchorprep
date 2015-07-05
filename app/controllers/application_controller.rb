e5b9568b932437bed74d4b25e421539bb5172b1f
Alex Triana <alex.triana2@gmail.com>
2015-07-05 10:26:34 -0700

    origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :allow_iframe_requests
 
   def after_sign_in_path_for(user)
   	admin_dashboard_path
   end
 
+  def iframe_action
+    response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM http://anchorprep.org"
+  end
 end
